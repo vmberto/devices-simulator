6c3120d4460d574f1c609d775b2d61cde92806e1
UmbertoB <umbertobarrosf@gmail.com>
2019-02-09 02:31:35 -0200

    [production] | 09/02/2019 - 02:31

--- dist/salvus-devices/main.js
+++ dist/salvus-devices/main.js
@@ -350,16 +350,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
 var environment = {
     production: false,
-    API_URL: 'http://localhost:5000'
+    API_URL: 'https://salvusserver.herokuapp.com'
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
 
 
 /***/ }),
