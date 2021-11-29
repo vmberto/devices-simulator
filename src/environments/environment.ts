c7e66dfe6aef4b2286bf4c21fa7d4d3418b8e923
UmbertoB <umbertobarrosf@gmail.com>
2021-11-29 13:49:11 -0300

    Dashboard url hash

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   API_URL: 'https://devices-service.herokuapp.com',
-  DASHBOARD_URL: 'https://devices-dashboard.herokuapp.com',
+  DASHBOARD_URL: 'https://devices-dashboard.herokuapp.com/#',
 };
